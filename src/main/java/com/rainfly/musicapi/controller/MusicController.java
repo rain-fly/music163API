12d52b7105a7814f992ef736f322d137a6451fbb
diaoy <1>
2019-07-04 12:51:34 +0800

    重构

--- src/main/java/com/rainfly/musicapi/controller/MusicController.java
+++ src/main/java/com/rainfly/musicapi/controller/MusicController.java
@@ -33,53 +33,8 @@ public class MusicController  {
 
     @RequestMapping("/getSongList")
     List<String> getSongList(String[] songId) throws Exception{
-        String scriptResult ="";//脚本的执行结果
-        //JSON用HASHMap分装
-        String ids = "";
-        for (int i =0;i<songId.length;i++){
-            if (i ==0){
-                ids = songId[0];
-            }else{
-                ids = ids + "," + songId[i];
-            }
-        }
-
-        //2.引擎读取 脚本字符串
-        //如果js存在文件里，举例
-        ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName("javascript");
-        System.out.println("读取crypto文件长度："+new File(resourcePath+"crypto-js.js").length());
-        System.out.println("读取core文件长度："+new File(resourcePath+"core.js").length());
-        engine.eval(new FileReader(new File(resourcePath+"crypto-js.js")));
-        engine.eval(new FileReader(new File(resourcePath+"core.js")));
-        engine.eval(new FileReader(new File(resourcePath+"Barrett.js")));
-        engine.eval(new FileReader(new File(resourcePath+"BigInt.js")));
-        engine.eval(new FileReader(new File(resourcePath+"RSA.js")));
-        Invocable invocable = (Invocable) engine;
-
-        //执行js函数
-        System.out.println("传入歌曲ids：***************"+ids+"**********************");
-        Map<String,String> test = (Map<String,String>) invocable.invokeFunction("getRouteInfo", "330706");
-        test.put("params",test.get("encText"));
-        test.remove("encText");
-
-        String url = "https://music.163.com/weapi/song/enhance/player/url/v1";
-        String jsonRetrun ="";
-        jsonRetrun = HttpClient.send(url, test,"utf-8");
-        System.out.println("请求网易云返回的数据：***************"+jsonRetrun+"**********************");
-        JsonParser jsonParser  = new JsonParser();
-        JsonObject jsonObject = (JsonObject) jsonParser.parse(jsonRetrun);
-        List<String>  list =  new ArrayList<>();
-        for (int i = 0;i<songId.length;i++){
-            list.add(String.valueOf(jsonObject.get("data").getAsJsonArray().get(i).getAsJsonObject().get("url")));
-        }
-         return list;
-     }
-
-    @RequestMapping("/getSongList2")
-    List<String> getSongList2(String[] songId) throws Exception {
         SongMP3Entity requestEntity = new SongMP3Entity();
-       // String[] ids = {"330706"};
+        // String[] ids = {"330706"};
         requestEntity.setIds(songId);
         requestEntity.setLevel("standard");
         requestEntity.setEncodeType("aac");
@@ -99,8 +54,7 @@ public class MusicController  {
             list.add(location);
         }
         return list;
-
-    }
+     }
 
 
 }
